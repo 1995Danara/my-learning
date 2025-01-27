4c948d01647099015a9d303a589018e7e462b915
Dana <dmitrievadanara@gmail.com>
2025-01-27 16:19:05 +0300

    changes

--- src/components/reviewsform/ReviewsForm.jsx
+++ src/components/reviewsform/ReviewsForm.jsx
@@ -1,12 +1,15 @@
 import { useReducer } from "react"
-
+import { Counter } from "../counter/Counter"
 const INITIAL_VALUE = {
   name: "",
   text: "",
-  rating: "",
+  rating: 0,
 }
+
 const SET_NAME_ACTION = "SET_NAME"
-const CLEAR_FORM = "CLEAR_FORM"
+const SET_TEXT_ACTION = "SET_TEXT"
+const SET_RATING_ACTION = "SET_RATING"
+const CLEAR_FORM_ACTION = "CLEAR_FORM"
 
 const reducer = (state, { type, payload }) => {
   switch (type) {
@@ -15,34 +18,45 @@ const reducer = (state, { type, payload }) => {
         ...state,
         name: payload,
       }
-    case "TEXT_ACTION":
+    case SET_TEXT_ACTION:
       return {
         ...state,
         text: payload,
       }
-    case "RATING_ACTION":
+    case SET_RATING_ACTION:
       return {
         ...state,
         rating: payload,
       }
-    case "CLEAR_FORM":
+    case CLEAR_FORM_ACTION:
       return INITIAL_VALUE
     default:
-      return {
-        state,
-      }
+      return state
   }
 }
 
 export const ReviewsForm = () => {
   const [form, dispatch] = useReducer(reducer, INITIAL_VALUE)
+
   const setName = (value) => dispatch({ type: SET_NAME_ACTION, payload: value })
-  const setText = (value) => dispatch({ type: "TEXT_ACTION", payload: value })
-  const setRating = (value) =>
-    dispatch({ type: "RATING_ACTION", payload: value })
-  const clearForm = () => dispatch({ type: CLEAR_FORM })
+  const setText = (value) => dispatch({ type: SET_TEXT_ACTION, payload: value })
+
+  const setRatingIncrement = () => {
+    if (form.rating < 5) {
+      dispatch({ type: SET_RATING_ACTION, payload: form.rating + 1 })
+    }
+  }
+
+  const setRatingDecrement = () => {
+    if (form.rating > 0) {
+      dispatch({ type: SET_RATING_ACTION, payload: form.rating - 1 })
+    }
+  }
+
+  const clearForm = () => dispatch({ type: CLEAR_FORM_ACTION })
 
   const { name, text, rating } = form
+
   return (
     <div
       style={{
@@ -70,18 +84,11 @@ export const ReviewsForm = () => {
       <div>
         <span>Rating</span>
         <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
-          {[1, 2, 3, 4, 5].map((value) => (
-            <label key={value}>
-              <input
-                type="radio"
-                name="rating"
-                value={value}
-                checked={rating === String(value)}
-                onChange={() => setRating(String(value))}
-              />
-              {value}
-            </label>
-          ))}
+          <Counter
+            count={rating}
+            onDecrement={setRatingDecrement}
+            onIncrement={setRatingIncrement}
+          />
         </div>
         <div>
           <button
